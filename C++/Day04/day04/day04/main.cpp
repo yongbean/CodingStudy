e073ede7610fe40452c4d0cd03dcb7e9b29d902e
yongbean <yongbeanchung@gmail.com>
2022-01-18 18:28:58 +0900

    update_220118

--- C++/Day04/day04/day04/main.cpp
+++ C++/Day04/day04/day04/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+private:
+    int speed;
+    int gear;
+    string color;
+    
+public:
+    int getSpeed();
+    void setSpeed(int s);
+    void setSpeed(double s);
+};
+
+int Car::getSpeed(){
+    return speed;
+}
+
+void Car::setSpeed(int s){
+    speed = s;
+}
+
+void Car::setSpeed(double s){
+    speed = (int)s;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Car myCar;
+    myCar.setSpeed(80);
+    myCar.setSpeed(85.5);
+    
+    cout << "차의 속도는" << myCar.getSpeed() << endl;
     return 0;
 }
